196993bc22bb97ba266d071f4d54464241dc773c
tubi55 <tubi55@nate.com>
2023-12-11 23:07:24 +0900

    map data fetching

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -3,10 +3,12 @@ import Layout from '../../common/layout/Layout';
 import './Contact.scss';
 
 export default function Contact() {
+	console.log('contact');
 	//get CDN Object
 	const kakao = useRef(window.kakao);
 
 	//states
+	const [Info, setInfo] = useState([]);
 	const [Index, setIndex] = useState(0);
 	const [Traffic, setTraffic] = useState(false);
 	const [View, setView] = useState(false);
@@ -14,86 +16,89 @@ export default function Contact() {
 	//refs
 	const mapFrame = useRef(null);
 	const viewFrame = useRef(null);
+	const map = useRef(null);
 	const marker = useRef(null);
-	const mapInstance = useRef(null);
 
-	//branch info
-	const mapInfo = useRef([
-		{
-			title: '삼성역 코엑스',
-			latlng: new kakao.current.maps.LatLng(37.51100661425726, 127.06162026853143),
-			imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
-			imgSize: new kakao.current.maps.Size(232, 99),
-			imgPos: { offset: new kakao.current.maps.Point(116, 99) },
-		},
-		{
-			title: '넥슨 본사',
-			latlng: new kakao.current.maps.LatLng(37.40211707077346, 127.10344953763003),
-			imgSrc: `${process.env.PUBLIC_URL}/img/marker2.png`,
-			imgSize: new kakao.current.maps.Size(232, 99),
-			imgPos: { offset: new kakao.current.maps.Point(116, 99) },
-		},
-		{
-			title: '서울 시청',
-			latlng: new kakao.current.maps.LatLng(37.5662952, 126.9779451),
-			imgSrc: `${process.env.PUBLIC_URL}/img/marker3.png`,
-			imgSize: new kakao.current.maps.Size(232, 99),
-			imgPos: { offset: new kakao.current.maps.Point(116, 99) },
-		},
-	]);
+	//get fetch data func
+	const getData = async () => {
+		const data = await fetch(process.env.PUBLIC_URL + '/DB/mapInfo.json');
+		const json = await data.json();
 
-	//marker instance
-	marker.current = new kakao.current.maps.Marker({
-		position: mapInfo.current[Index].latlng,
-		image: new kakao.current.maps.MarkerImage(mapInfo.current[Index].imgSrc, mapInfo.current[Index].imgSize, mapInfo.current[Index].imgOpt),
-	});
+		setInfo(
+			json.mapInfo.map(el => ({
+				title: el.title,
+				latlng: new kakao.current.maps.LatLng(...el.latlng),
+				imgSrc: `${process.env.PUBLIC_URL}/img/${el.imgSrc}`,
+				imgSize: new kakao.current.maps.Size(...el.imgSize),
+				imgPos: { offset: new kakao.current.maps.Point(...el.imgPos) }
+			}))
+		);
+	};
 
-	//get roadview instance func
+	//create mapInstance func
+	const createMap = () => {
+		//set ref, map, marker
+		map.current = new kakao.current.maps.Map(mapFrame.current, { center: Info[Index].latlng });
+		marker.current = new kakao.current.maps.Marker({
+			position: Info[Index].latlng,
+			image: new kakao.current.maps.MarkerImage(Info[Index].imgSrc, Info[Index].imgSize, Info[Index].imgPos)
+		});
+		marker.current.setMap(map.current);
+
+		//add controller
+		map.current.addControl(new kakao.current.maps.MapTypeControl(), kakao.current.maps.ControlPosition.TOPRIGHT);
+		map.current.addControl(new kakao.current.maps.ZoomControl(), kakao.current.maps.ControlPosition.RIGHT);
+		map.current.setZoomable(false);
+
+		//window event
+		window.addEventListener('resize', setCenter);
+		return () => window.removeEventListener('resize', setCenter);
+	};
+
+	//get roadview func
 	const roadview = () => {
-		new kakao.current.maps.RoadviewClient().getNearestPanoId(mapInfo.current[Index].latlng, 50, (panoId) => {
-			new kakao.current.maps.Roadview(viewFrame.current).setPanoId(panoId, mapInfo.current[Index].latlng);
+		new kakao.current.maps.RoadviewClient().getNearestPanoId(Info[Index].latlng, 50, panoId => {
+			new kakao.current.maps.Roadview(viewFrame.current).setPanoId(panoId, Info[Index].latlng);
 		});
 	};
 
-	//map pos init func
+	//init map pos func
 	const setCenter = () => {
-		mapInstance.current.setCenter(mapInfo.current[Index].latlng);
+		map.current.setCenter(Info[Index].latlng);
 		roadview();
 	};
 
-	//Map Render with Index
+	//fetch data once
+	useEffect(() => getData(), []);
+
+	//init render with fetched data
+	useEffect(() => Info[Index] && createMap(), [Info]);
+
+	//re-render with Index
 	useEffect(() => {
+		if (!Info[Index]) return;
 		mapFrame.current.innerHTML = '';
-		mapInstance.current = new kakao.current.maps.Map(mapFrame.current, { center: mapInfo.current[Index].latlng });
-		marker.current.setMap(mapInstance.current);
+		createMap();
 		setTraffic(false);
 		setView(false);
 		roadview();
-
-		mapInstance.current.addControl(new kakao.current.maps.MapTypeControl(), kakao.current.maps.ControlPosition.TOPRIGHT);
-		mapInstance.current.addControl(new kakao.current.maps.ZoomControl(), kakao.current.maps.ControlPosition.RIGHT);
-		mapInstance.current.setZoomable(false);
-
-		window.addEventListener('resize', setCenter);
-		return () => window.removeEventListener('resize', setCenter);
 	}, [Index]);
 
-	//Toggle Render with Traffic
+	//re-render with Traffic
 	useEffect(() => {
 		Traffic
-			? mapInstance.current.addOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC)
-			: mapInstance.current.removeOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC);
+			? map.current?.addOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC)
+			: map.current?.removeOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC);
 	}, [Traffic]);
 
 	return (
 		<Layout title={'Contact'}>
 			<div className='controlBox'>
 				<nav className='branch'>
-					{mapInfo.current.map((el, idx) => (
-						<button key={idx} onClick={() => setIndex(idx)} className={idx === Index ? 'on' : ''}>
-							{el.title}
-						</button>
-					))}
+					{Info.map((el, idx) =>
+						//prettier-ignore
+						<button key={idx} onClick={() => setIndex(idx)} className={idx === Index ? 'on' : ''}>{el.title}</button>
+					)}
 				</nav>
 
 				<nav className='info'>
