6e6b5bf400fb7cfcc007ce93b9a263aa7fde974a
tubi55 <tubi55@nate.com>
2023-12-11 16:04:51 +0900

    날짜 객체 없을떄 오류 해결

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -6,7 +6,6 @@ import { TfiWrite } from 'react-icons/tfi';
 import { useCustomText } from '../../../hooks/useText';
 
 export default function Community() {
-	console.log('community');
 	const changeText = useCustomText('combined');
 	const getLocalData = () => {
 		const data = localStorage.getItem('post');
@@ -14,17 +13,17 @@ export default function Community() {
 		else return [];
 	};
 	const [Post, setPost] = useState(getLocalData());
-	const [CurNum, setCurNum] = useState(0); //페이징 버튼 클릭시 현재 보일 페이지 번호가 담길 state
-	const [PageNum, setPageNum] = useState(0); //전체 PageNum이 담길 state
+	const [CurNum, setCurNum] = useState(0);
+	const [PageNum, setPageNum] = useState(0);
 
 	const refTit = useRef(null);
 	const refCon = useRef(null);
 	const refEditTit = useRef(null);
 	const refEditCon = useRef(null);
 	const editMode = useRef(false);
-	const len = useRef(0); //전체 Post갯수를 담을 참조 객체
-	const pageNum = useRef(0); //전체 페이지 갯수를 추후에 연산해서 담을 참조객체
-	const perNum = useRef(6); //한 페이지당 보일 포스트 갯수
+	const len = useRef(0);
+	const pageNum = useRef(0);
+	const perNum = useRef(6);
 
 	//input 초기화 함수
 	const resetPost = () => {
@@ -40,7 +39,11 @@ export default function Community() {
 		}
 		const korTime = new Date().getTime() + 1000 * 60 * 60 * 9;
 		setPost([
-			{ title: refTit.current.value, content: refCon.current.value, date: new Date(korTime) },
+			{
+				title: refTit.current.value,
+				content: refCon.current.value,
+				date: new Date(korTime),
+			},
 			...Post,
 		]);
 		resetPost();
@@ -97,42 +100,25 @@ export default function Community() {
 		);
 	};
 
-	const filtering = (txt) => {
-		const abc = Post.filter((el) => el.title.indexOf(txt) >= 0 || el.content.indexOf(txt) >= 0);
-		console.log(abc);
-	};
-
 	useEffect(() => {
 		Post.map((el) => (el.enableUpdate = false));
 		localStorage.setItem('post', JSON.stringify(Post));
 
 		len.current = Post.length;
 
-		pageNum.current =
-			len.current % perNum.current === 0
-				? len.current / perNum.current
-				: parseInt(len.current / perNum.current) + 1;
+		pageNum.current = len.current % perNum.current === 0 ? len.current / perNum.current : parseInt(len.current / perNum.current) + 1;
 
-		//새로고침했을때 페이징 버튼이 안뜨는 문제
-		//원인 : 현재 로직이 Post값자체게 변경되면 pageNum.current값이 변경되게 하고 있는데..
-		//pageNum.current가 변경되고 state가 아니기 때문에 화면을 자동 재랜더링하지 않는 문제 발생
-		//해결방법 : 만들어진 참조객체값을 state PageNum에 옮겨담음
 		setPageNum(pageNum.current);
 	}, [Post]);
 
 	return (
 		<Layout title={'Community'}>
-			{/* 위에서 만든 pageNum값을 활용해 자동으로 페이지버튼 생성 */}
 			<nav className='pagination'>
 				{Array(PageNum)
 					.fill()
 					.map((_, idx) => {
 						return (
-							<button
-								key={idx}
-								onClick={() => idx !== CurNum && setCurNum(idx)}
-								className={idx === CurNum ? 'on' : ''}
-							>
+							<button key={idx} onClick={() => idx !== CurNum && setCurNum(idx)} className={idx === CurNum ? 'on' : ''}>
 								{idx + 1}
 							</button>
 						);
@@ -157,7 +143,8 @@ export default function Community() {
 				<div className='showBox'>
 					{Post.map((el, idx) => {
 						const date = JSON.stringify(el.date);
-						const strDate = changeText(date.split('T')[0].slice(1), '.');
+						//처음 마운트시 빈배열안에 date객체아 없으므로 chageText훅 오류 발생하므로 해당 값이 있을때 호출
+						const strDate = date && changeText(date.split('T')[0].slice(1), '.');
 
 						if (idx >= perNum.current * CurNum && idx < perNum.current * (CurNum + 1)) {
 							return (
@@ -200,33 +187,3 @@ export default function Community() {
 		</Layout>
 	);
 }
-
-/*
-	1.글입력 박스, 글 출력박스를 생성
-	2.전체글을 관리할 배열 state를 생성 [{글정보1}, {글정보2}, {글정보3}]
-	3.글입력박스에 글 입력후 저장 버튼 클릭시 글 정보를 객체형태로 state계속 추가 (Create)
-	4.state 배열에 추가된값들을 반복돌면서 글 리스트 출력 (Read)
-	5.글출력시 삭제, 수정버튼 추가해서 출력
-	6.글리스트에서 삭제 버튼 클릭시 배열State에서 이벤트가 발생한 순번의 객체를 제거해서 글삭제 (Delete)
-
-	Create (데이터저장) 글작성
-	Read (데이터호출) 글목록 보기
-	Update (데이터변경) 글 수정
-	Delete (데이터삭제) 글 삭제
-
-	LocalStorage: 모든 브라우저가 내장하고 있는 경량의 저장소
-	-- 문자값만 저장가능 (5MB)
-	-- 로컬저장소에 문자열이외의 값을 저장할때에는 강제로 문자화시켜서 저장
-	-- 로컬저장소의 값을 JS로 가져올떄에는 문자값을 반대로 객체화시켜서 호출
-
-	localStorage객체에 활용가능한 메서드
-	- setItem('키','문자화된 데이터'); 해당 키값에 데이터를 담아서 저장
-	- getItem('키') : 해당 키값에 매칭이 되는 데이터를 가져옴
-
-	글 수정 로직 단계
-	1. 각 포스트에서 수정 버튼 클릭시 해당 객체에 enableUpdat=true라는 프로퍼티를 동적으로 추가후 state저장
-	2. 다음번 렌더링 사이클에서 포스트를 반복돌며 객체에 enableUpate값이 true이면을 제목 본문을 input요소에 담아서 출력하도록 분기처리 (출력시 수정모드로 분기처리해서 출력)
-	3. 수정모드일때는 수정취소, 수정완료 버튼 생성
-	4, 수정모드에서 수정취소 버튼 클릭시 해당 포스트 객체에 enableUpdate=false로 변경해서 다시 출력모드 변경
-	5. 수정모드에서 수정완료 버튼 클릭시 해당 폼요소에 수정된 value값을 가져와서 저장한뒤 다시 출력모드 변경
-*/
