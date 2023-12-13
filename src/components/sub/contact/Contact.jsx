c5af125b7a71b33475f7e1b6bca3eef0f6ab45aa
tubi55 <tubi55@nate.com>
2023-12-13 09:33:13 +0900

    window cdn객체를 hook에서 미리 받아서 state반환하도록 변경

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -4,103 +4,103 @@ import './Contact.scss';
 import useScript from '../../../hooks/useScript';
 
 export default function Contact() {
-	const setScript = useScript();
+	const Script = useScript(`//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${process.env.REACT_APP_KAKAO_API}`, 'kakao');
+
+	console.log(Script);
+
+	useEffect(() => {
+		if (Script.kakao) {
+			console.log(Script);
+			//Script.kakao.maps.load(() => getData(Script.kakao));
+		}
+	}, [Script]);
 
 	//states
-	const [kakao, setKakao] = useState(null);
-	const [Info, setInfo] = useState([]);
-	const [Index, setIndex] = useState(0);
-	const [Traffic, setTraffic] = useState(false);
-	const [View, setView] = useState(false);
+	//const [Info, setInfo] = useState([]);
+	//console.log(Info);
+	//const [Index, setIndex] = useState(0);
+	//const [Traffic, setTraffic] = useState(false);
+	//const [View, setView] = useState(false);
 
 	//refs
-	const mapFrame = useRef(null);
-	const viewFrame = useRef(null);
-	const map = useRef(null);
-	const marker = useRef(null);
-
-	//get cdn script func
-	const getCDN = async () => {
-		await setScript(`//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${process.env.REACT_APP_KAKAO_API}`);
-		const kakao = window.kakao;
-		setKakao(kakao);
-		kakao.maps.load(() => getData(kakao));
-	};
+	//const mapFrame = useRef(null);
+	//const viewFrame = useRef(null);
+	//const map = useRef(null);
+	//const marker = useRef(null);
 
 	//get fetch data func
-	const getData = async kakao => {
-		console.log('getData');
-		const data = await fetch(process.env.PUBLIC_URL + '/DB/mapInfo.json');
-		const json = await data.json();
+	// const getData = async kakao => {
+	// 	const data = await fetch(process.env.PUBLIC_URL + '/DB/mapInfo.json');
+	// 	const json = await data.json();
 
-		setInfo(
-			json.mapInfo.map(el => ({
-				title: el.title,
-				latlng: new kakao.maps.LatLng(...el.latlng),
-				imgSrc: `${process.env.PUBLIC_URL}/img/${el.imgSrc}`,
-				imgSize: new kakao.maps.Size(...el.imgSize),
-				imgPos: { offset: new kakao.maps.Point(...el.imgPos) }
-			}))
-		);
-	};
+	// 	setInfo(
+	// 		json.mapInfo.map(el => ({
+	// 			title: el.title,
+	// 			latlng: new kakao.maps.LatLng(...el.latlng),
+	// 			imgSrc: `${process.env.PUBLIC_URL}/img/${el.imgSrc}`,
+	// 			imgSize: new kakao.maps.Size(...el.imgSize),
+	// 			imgPos: { offset: new kakao.maps.Point(...el.imgPos) }
+	// 		}))
+	// 	);
+	// };
 
 	//create mapInstance func
-	const createMap = () => {
-		//set ref, map, marker
-		map.current = new kakao.maps.Map(mapFrame.current, { center: Info[Index].latlng });
-		marker.current = new kakao.maps.Marker({
-			position: Info[Index].latlng,
-			image: new kakao.maps.MarkerImage(Info[Index].imgSrc, Info[Index].imgSize, Info[Index].imgPos)
-		});
-		marker.current.setMap(map.current);
+	// const createMap = () => {
+	// 	//set ref, map, marker
+	// 	map.current = new kakao.maps.Map(mapFrame.current, { center: Info[Index].latlng });
+	// 	marker.current = new kakao.maps.Marker({
+	// 		position: Info[Index].latlng,
+	// 		image: new kakao.maps.MarkerImage(Info[Index].imgSrc, Info[Index].imgSize, Info[Index].imgPos)
+	// 	});
+	// 	marker.current.setMap(map.current);
 
-		//add controller
-		map.current.addControl(new kakao.maps.MapTypeControl(), kakao.maps.ControlPosition.TOPRIGHT);
-		map.current.addControl(new kakao.maps.ZoomControl(), kakao.maps.ControlPosition.RIGHT);
-		map.current.setZoomable(false);
+	// 	//add controller
+	// 	map.current.addControl(new kakao.maps.MapTypeControl(), kakao.maps.ControlPosition.TOPRIGHT);
+	// 	map.current.addControl(new kakao.maps.ZoomControl(), kakao.maps.ControlPosition.RIGHT);
+	// 	map.current.setZoomable(false);
 
-		//window event
-		window.addEventListener('resize', setCenter);
-		return () => window.removeEventListener('resize', setCenter);
-	};
+	// 	//window event
+	// 	window.addEventListener('resize', setCenter);
+	// 	return () => window.removeEventListener('resize', setCenter);
+	// };
 
 	// //get roadview func
-	const roadview = () => {
-		new kakao.maps.RoadviewClient().getNearestPanoId(Info[Index].latlng, 50, panoId => {
-			new kakao.maps.Roadview(viewFrame.current).setPanoId(panoId, Info[Index].latlng);
-		});
-	};
+	// const roadview = () => {
+	// 	new kakao.maps.RoadviewClient().getNearestPanoId(Info[Index].latlng, 50, panoId => {
+	// 		new kakao.maps.Roadview(viewFrame.current).setPanoId(panoId, Info[Index].latlng);
+	// 	});
+	// };
 
 	// //init map pos func
-	const setCenter = () => {
-		map.current.setCenter(Info[Index].latlng);
-		roadview();
-	};
+	// const setCenter = () => {
+	// 	map.current.setCenter(Info[Index].latlng);
+	// 	roadview();
+	// };
 
 	//init CDN
-	useEffect(() => getCDN(), []);
+	// useEffect(() => getCDN(), []);
 
 	//init render with fetched data
-	useEffect(() => Info[Index] && createMap(), [Info]);
+	// useEffect(() => Info[Index] && createMap(), [Info]);
 
 	//re-render with Index
-	useEffect(() => {
-		if (!Info[Index]) return;
-		mapFrame.current.innerHTML = '';
-		createMap();
-		setTraffic(false);
-		setView(false);
-		roadview();
-	}, [Index]);
+	// useEffect(() => {
+	// 	if (!Info[Index]) return;
+	// 	mapFrame.current.innerHTML = '';
+	// 	createMap();
+	// 	setTraffic(false);
+	// 	setView(false);
+	// 	roadview();
+	// }, [Index]);
 
 	//re-render with Traffic
-	useEffect(() => {
-		Traffic ? map.current?.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC) : map.current?.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
-	}, [Traffic]);
+	// useEffect(() => {
+	// 	Traffic ? map.current?.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC) : map.current?.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+	// }, [Traffic]);
 
 	return (
 		<Layout title={'Contact'}>
-			<div className='controlBox'>
+			{/* <div className='controlBox'>
 				<nav className='branch'>
 					{Info.map((el, idx) =>
 						//prettier-ignore
@@ -118,7 +118,7 @@ export default function Contact() {
 			<section className='tab'>
 				<article className={`mapBox ${View ? '' : 'on'}`} ref={mapFrame}></article>
 				<article className={`viewBox ${View ? 'on' : ''}`} ref={viewFrame}></article>
-			</section>
+			</section> */}
 		</Layout>
 	);
 }
--- src/hooks/useScript.js
+++ src/hooks/useScript.js
@@ -1,3 +1,28 @@
+import { useState, useEffect } from 'react';
+
+const useScript = (url, name) => {
+	const [Lib, setLib] = useState(null);
+
+	useEffect(() => {
+		const script = document.createElement('script');
+		script.async = true;
+		script.src = url;
+		script.onload = () => {
+			setLib({ [name]: window[name] });
+		};
+
+		document.head.appendChild(script);
+
+		return () => {
+			document.head.removeCHild(script);
+		};
+	}, [url, name]);
+	return Lib;
+};
+
+export default useScript;
+
+/*
 const useScript = () => {
 	return url => {
 		return new Promise((res, rej) => {
@@ -15,3 +40,4 @@ const useScript = () => {
 };
 
 export default useScript;
+*/
