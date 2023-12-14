0758a8288583397ea1b9fbc0b9e5ce4977b68deb
tubi55 <tubi55@nate.com>
2023-12-14 09:30:02 +0900

    cdn script 동적 생성

--- src/hooks/useScript.js
+++ src/hooks/useScript.js
@@ -7,37 +7,12 @@ const useScript = (url, name) => {
 		const script = document.createElement('script');
 		script.async = true;
 		script.src = url;
-		script.onload = () => {
-			setLib({ [name]: window[name] });
-		};
+		script.onload = () => setLib({ [name]: window[name] });
 
 		document.head.appendChild(script);
-
-		return () => {
-			document.head.removeCHild(script);
-		};
+		return () => document.head.removeCHild(script);
 	}, [url, name]);
 	return Lib;
 };
 
 export default useScript;
-
-/*
-const useScript = () => {
-	return url => {
-		return new Promise((res, rej) => {
-			const script = document.createElement('script');
-			script.src = url;
-			script.addEventListener('load', () => {
-				res();
-			});
-			script.addEventListener('error', e => {
-				rej(e);
-			});
-			document.head.appendChild(script);
-		});
-	};
-};
-
-export default useScript;
-*/
